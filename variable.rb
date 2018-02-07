74ebaae7b50cffae8fc5e9feffe52c44d56329e5
Dan Winter <daniel.winter@wework.com>
2018-02-07 00:48:51 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,5 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 
-anew variable = 1
-Greeting = "Hello World"
+anewvariable = 1
+greeting = "Hello World"
